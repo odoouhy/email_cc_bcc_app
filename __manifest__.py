3b065233645298aee53a5b73eb5e65ad8bf59eaf
Allan Ramirez <aramirez@exeticsoluciona.com>
2022-03-07 22:31:27 -0600

    [upd]

--- __manifest__.py
+++ __manifest__.py
@@ -6,10 +6,6 @@
     "category" : "Email",
     'summary': 'App for send Email to customer with CC and BCC',
     "description": """
-    App for send Email to customer with CC and BCC, manage customer email cc bcc. Add CC/BCC in Email settings.
-                    cc bcc on email wizard, email wizard cc and bcc, CC and BCC option on email wizard. Email CC and BCC option.
-    Email CC BCC ODOO Email CC and BCC Mail Compose with Email CC compose Mail with Email CC Compose Email CC email Cc and Bcc cc email cc bcc email email multiple Cc and Bcc Add CC/BCC in Compose Email CC BCC Compose Email Add CC/BCC in Compose Email add multiple reciept email reply to all email reply to all email Default CC/BCC customer email sending option for send email mail send option cc mail bcc mail
-
                 """,
     "license" : "OPL-1",
     "depends" : ['mail', 'account'],
